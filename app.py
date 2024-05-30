468518854119fe86a41dfc1cb43338f761fe226a
Your Name <you@example.com>
2024-05-30 09:00:30 -0300

    minor changes

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
 
 @app.route('/users', methods=['GET'])
 def get_user():
