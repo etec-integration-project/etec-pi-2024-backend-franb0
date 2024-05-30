3a05eed4b4a6b9ba54a7510703ef9719b946ee1d
Your Name <you@example.com>
2024-05-30 08:42:58 -0300

    minor changes

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
