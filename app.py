d855c76a27eeab9eb41c42a09ee2c1ebe7641959
Your Name <you@example.com>
2024-05-30 08:47:02 -0300

    external class

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from models import User
 
 load_dotenv()
 
@@ -12,18 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'email': self.email
-        }
 
 @app.route('/users', methods=['GET'])
 def get_users():
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -10,6 +11,5 @@ class User(db.Model):
         return {
             'id': self.id,
             'name': self.name,
-            'email': self.email,
-            'createdAt': self.createdAt.strftime('%Y-%m-%d %H:%M:%S')  # Format the datetime object
+            'email': self.email
         }
