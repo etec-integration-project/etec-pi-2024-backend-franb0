63748d354cd5b4b755c24b3f4f4e080265237e8c
Your Name <you@example.com>
2024-05-30 08:30:44 -0300

    minor changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from datetime import datetime
 import os
 
 load_dotenv()
@@ -13,36 +12,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = Users.query.all()
+    users = User.query.all()
     return jsonify([user.to_dict() for user in users])
 
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    user = Users.query.get_or_404(id)
+    user = User.query.get_or_404(id)
     return jsonify(user.to_dict())
 
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.json
-    new_user = Users(name=data['name'], email=data['email'], password=data['password'])
+    new_user = User(name=data['name'], email=data['email'], password=data['password'])
     db.session.add(new_user)
     db.session.commit()
     return jsonify(new_user.to_dict()), 201
 
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    user = Users.query.get_or_404(id)
+    user = User.query.get_or_404(id)
     data = request.json
     user.name = data['name']
     user.email = data['email']
@@ -52,7 +48,7 @@ def update_user(id):
 
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    user = Users.query.get_or_404(id)
+    user = User.query.get_or_404(id)
     db.session.delete(user)
     db.session.commit()
     return jsonify({'message': 'User deleted'})
