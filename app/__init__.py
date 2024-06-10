405a0c4c5a130bb831ad5bdba65c06eea4241fb9
franb0 <128163570+franb0@users.noreply.github.com>
2024-06-10 19:03:04 -0300

    swapped app.py for init

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from __init__.py import create_app
 
-db = SQLAlchemy()
+app = create_app()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql/DATABASE_NAME'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-
-    with app.app_context():
-        # Import parts of our application
-        from . import routes
-        from . import models
-
-        # Create database tables
-        db.create_all()
-
-    return app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
